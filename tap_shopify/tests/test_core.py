d99cdb5f885e681cf0207d248c93cd4e66fc1e7a
Aaron Phethean <aphethean@matatika.com>
2022-02-28 12:26:26 +0000

    Add mocked responses to ensure basic core unit tests pass

--- tap_shopify/tests/test_core.py
+++ tap_shopify/tests/test_core.py
@@ -1,6 +1,8 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
+import unittest
 import datetime
+import responses
 
 from singer_sdk.testing import get_standard_tap_tests
 
@@ -12,15 +14,35 @@ SAMPLE_CONFIG = {
     "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
 }
 
-# Run standard built-in tap tests from the SDK:
-def test_standard_tap_tests():
-    """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(
-        Taptap_shopify,
-        config=SAMPLE_CONFIG
-    )
-    for test in tests:
-        test()
+class TestCore(unittest.TestCase):
+    """Test class for core tap tests"""
 
+    def setUp(self):
+        # reset mock responses
+        responses.reset()
 
-# TODO: Create additional tests as appropriate for your tap.
+    # Run standard built-in tap tests from the SDK:
+    @responses.activate
+    def test_standard_tap_tests(self):
+        """Run standard tap tests from the SDK."""
+        # given a mock response to the standard stream test
+        responses.add(
+            responses.GET,
+            "https://mock-store.myshopify.com/admin/api/2022-01/orders.json?status=any",
+            json={},
+            status=200,
+        )
+        responses.add(
+            responses.GET,
+            "https://mock-store.myshopify.com/admin/api/2022-01/products.json",
+            json={},
+            status=200,
+        )
+        # when run standard tests
+        tests = get_standard_tap_tests(
+            Taptap_shopify,
+            config=SAMPLE_CONFIG
+        )
+        # expect no failures
+        for test in tests:
+            test()
