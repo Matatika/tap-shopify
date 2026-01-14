78f9fc3073078b99cb1075f6f534d872f880c022
Reuben Frankel <rfrankel@matatika.com>
2026-01-14 16:07:46 +0000

    Fix line too long lint error

--- tap_shopify/__init__.py
+++ tap_shopify/__init__.py
@@ -3,7 +3,8 @@
 from collections import UserDict
 
 
-# important that this doesn't subclass dict directly to ensure json.dumps calls do not expose the contents
+# important that this doesn't subclass dict directly to ensure json.dumps calls do not
+# expose the contents
 class hiddendict(UserDict):
     def __repr__(self):
         return "***"
