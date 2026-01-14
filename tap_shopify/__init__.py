5c292abe29d7c052d933b655e5678c1a55c77aa8
Reuben Frankel <rfrankel@matatika.com>
2026-01-14 21:18:21 +0000

    Add docstring

--- tap_shopify/__init__.py
+++ tap_shopify/__init__.py
@@ -6,5 +6,7 @@ from collections import UserDict
 # important that this doesn't subclass dict directly to ensure json.dumps calls do not
 # expose the contents
 class hiddendict(UserDict):
+    """Dict-like object that does not expose contents when serialised."""
+
     def __repr__(self):
         return "***"
